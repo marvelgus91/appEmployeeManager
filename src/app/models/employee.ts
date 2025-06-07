ae89987d93c7526f2ab2bf417d949b62c60c8a25
Gustavo Leyva <marvelgus9@gmail.com>
2025-06-06 20:41:34 -0700

    Modify[employeeDetail]

--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -14,8 +14,11 @@ export interface Employee {
   id: number;
   fullName: string;
   company_id: number;
+  company: string;
   company_area_id: number;
+  companyArea: string;
   company_department_id: number;
+  companyDepartment: string;
   position: string;
   photo: string;
   startDate: Date;
